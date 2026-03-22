3d536f4740125135acd9ee7737e5b1623c7ab69b
pablof7z <pfer@me.com>
2026-03-22 11:03:51 +0000

    feat: add replica NIP-05 identifier

--- api/nip05.js
+++ api/nip05.js
@@ -5,7 +5,8 @@ export default function handler(req, res) {
 
   res.status(200).json({
     names: {
-      _: 'fa984bd7dbb282f07e16e7ae87b26a2a7b9b90b7246a44771f0cf5ae58018f52'
+      _: 'fa984bd7dbb282f07e16e7ae87b26a2a7b9b90b7246a44771f0cf5ae58018f52',
+      replica: '4108cd882d5bd7446b4b5cb0688b14694f3d0dbb52bd24f16e1e29ff1636adab'
     }
   });
 }
